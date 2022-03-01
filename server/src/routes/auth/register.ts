d0e7226cf09c985792dd68d46d5b83db6e45f36f
Alex Aranda <alex.aranda@vincit.fi>
2022-02-28 21:23:57 -0800

    Seperated repo into client and server directories

---
+++ server/src/routes/auth/register.ts
@@ -0,0 +1,55 @@
+import { hash } from "bcrypt";
+import { Router } from "express";
+import { signJWT } from "../../services/jwt";
+import { getUsersByUserNameAndEmail, insertUser } from "../../services/queries";
+require("dotenv").config();
+
+const router = Router();
+
+router.use("/", async (req, res) => {
+  try {
+    const { email, userName, password } = req.body;
+    if (!email || !userName || !password) {
+      return res
+        .status(400)
+        .send({ email, userName, password, status: "Missing input" });
+    }
+
+    const existingUser = await getUsersByUserNameAndEmail({ email, userName });
+
+    if (existingUser[0]?.user_name === userName.toLowerCase()) {
+      return res.send({ error: "User name taken" });
+    } else if (existingUser[0]?.email === email.toLowerCase()) {
+      return res.send({ error: "Eror creating account" });
+    } else {
+      hash(password, 10, async (error, hashedPassword) => {
+        if (error) {
+          console.log({ error });
+        }
+        const user = await insertUser({
+          userName,
+          email,
+          password: hashedPassword,
+        });
+
+        if (user.length) {
+          const token = signJWT({
+            user_id: user[0].id,
+            email: user[0].email,
+            userName: user[0].user_name,
+            expiresIn: "2h",
+          });
+          return res
+            .status(201)
+            .send({ token, status: "Account created successfully" });
+        } else {
+          return res.status(400).send({ error: "Something went wrong" });
+        }
+      });
+    }
+  } catch (error) {
+    console.log({ error });
+  }
+});
+
+export default router;
---
+++ server/src/services/queries.ts
@@ -0,0 +1,40 @@
+import { knex } from "../config/knex";
+
+const lower = (text: string) => text.toLowerCase();
+
+export const getUsersByUserNameAndEmail = async ({
+  email,
+  userName,
+}: {
+  email: string;
+  userName: string;
+}) => {
+  return await knex
+    .select("email", "user_name")
+    .from("users")
+    .where("email", lower(email))
+    .orWhere("user_name", lower(userName));
+};
+
+export const insertUser = async ({
+  userName,
+  email,
+  password,
+}: {
+  userName: string;
+  email: string;
+  password: string;
+}) => {
+  return await knex("users").insert(
+    {
+      user_name: lower(userName),
+      email: lower(email),
+      password: password,
+    },
+    ["id", "email", "user_name"]
+  );
+};
+
+export const fetchUserByUserName = async (userName: string) => {
+  return await knex.select().from("users").where("user_name", lower(userName));
+};
